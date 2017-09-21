267e8e111bd48a00962d6cf4ee48ad6a7b649970
Mickey248 <mickeynaja246@gmail.com>
2017-09-21 23:27:41 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -109,6 +109,7 @@ return [
         'client' => 'predis',
 
         'default' => [
+            //id password localhost phpmyadmin
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
